3e13f20a6ad5be8a89272aabdbc7e112359630bd
Ellen Arteca <ellen.arteca@gmail.com>
2020-11-02 16:19:41 -0500

    test list fix

--- batchListOfTests.sh
+++ batchListOfTests.sh
@@ -17,12 +17,12 @@ fi
 for (( x=0; x<$warmups; x++ )); do
 	echo "Warmup run: "$x" of "$warmups
 	# run some tests into the void 
-	npm run test -- --o
+	npm run test -- 
 done
 
 for x in $(eval echo {1..$1}); do
 	echo "Running test suite: " $x
-	npm run test -- --o --runTestsByPath $(cat $2) >> $3 
+	npm run test -- --runTestsByPath $(cat $2) >> $3 
 	sed -rin "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" $3
 	python process_jest_xml_out.py $4
 	echo "Done running test suite, cleaning up now..."
